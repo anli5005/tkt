9a80acd745d83a7f09ecf0baa7c6b85da4d327bc
Anthony Li <li.anthony917@gmail.com>
2020-02-02 19:54:52 -0500

    Switch to dotenv

--- db/db.js
+++ db/db.js
@@ -1,12 +1,10 @@
 "use strict";
 
 const { Pool } = require("pg");
-const config = require("config");
+require("dotenv").config();
 
 let poolConfig = {};
-if (config.has("databaseUrl")) {
-    poolConfig.connectionString = config.get("databaseUrl");
-}
+poolConfig.connectionString = process.env.NOW_GITHUB_COMMIT_REF === "master" ? process.env.PROD_DATABASE_URL : process.env.DATABASE_URL;
 
 let pool = new Pool(poolConfig);
 
--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 module.exports = {
     webpack(config) {
         config.module.rules.push({
