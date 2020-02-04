a4974d8d4255a2508a9cd4f8283fd3e671e89a50
Anthony Li <li.anthony917@gmail.com>
2020-02-03 22:00:46 -0500

    Add connection limit

--- db/db.js
+++ db/db.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 
 let poolConfig = {};
 poolConfig.connectionString = process.env.NOW_GITHUB_COMMIT_REF === "master" ? process.env.PROD_DATABASE_URL : process.env.DATABASE_URL;
+poolConfig.max = parseInt(process.env.NOW_GITHUB_COMMIT_REF === "master" ? process.env.PROD_DATABASE_MAX_CONNECTIONS : process.env.DATABASE_MAX_CONNECTIONS) || 10;
 
 let pool = new Pool(poolConfig);
 
